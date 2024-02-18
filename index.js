50245c9510d5def164a4d6c6dc86728ec48e61f7
Jona Rumberg <jona@rumberg-online.de>
2024-02-18 13:36:18 +0100

    add post endpoint for authors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import { JSONFilePreset } from 'lowdb/node'
-import express from 'express'
+import { JSONFilePreset } from 'lowdb/node';
+import express from 'express';
+import {v4 as uuid } from 'uuid';
 
 const app = express()
+app.use(express.json()) // for parsing application/json
 
 const PORT = 8080;
 
@@ -10,29 +12,78 @@ const defaultData = {
 	authors: [],
 	categories: []
 }
+
+const model = {
+	books: {},
+	authors: {
+		name: (a) => typeof a == 'string',
+		birthdate: (a) => true
+	},
+	categories: {}
+}
+
+
 const db = await JSONFilePreset('db.json', defaultData)
 
 //Books endpoints
 app.get('/books', async (_req, res) => {
 	console.log("GET Books");
 
-	res.send(db.data.books);
+	res.status(200).send(db.data.books);
 })
 
 //Autors endpoints
 app.get('/authors', async (_req, res) => {
 	console.log("GET Authors");
 
-	res.send(db.data.authors);
+	res.status(200).send(db.data.authors);
+})
+
+app.post('/authors', async (req, res) => {
+	console.log("POST Authors");
+
+	if(validate(req.body, model.authors)){
+
+		req.body.id = uuid();
+
+		console.log("Inserting into db")
+		console.log(req.body)
+
+		db.data.authors.push(req.body);
+		db.write()
+		res.sendStatus(201);
+	} else {
+		console.log("Bad request")
+
+		res.sendStatus(400);
+	}
 })
 
 //Categories endpoints
 app.get('/categories', async (_req, res) => {
 	console.log("GET Categories");
 
-	res.send(db.data.categories);
+	res.status(200).send(db.data.categories);
 })
 
 app.listen(PORT, () => {
 	console.log("Server is running on port: " + PORT)
 })
+
+
+
+
+//function to iterate over input object and check against validator functions
+function validate(obj, args) {
+	// iterate over fields
+	for (const [key, validator] of Object.entries(args)) {
+		if (obj[key] == undefined) {
+			return false;
+		}
+		if (!validator(obj[key])) {
+			return false;
+		}
+	}
+
+	return true;
+}
